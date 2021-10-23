beef29b9cd59064698110fcbb84cde22f78817d0
Uttamgopal2910 <77666228+Uttamgopal2910@users.noreply.github.com>
2021-10-23 18:33:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAUjQQj14KDU1qG0no0zeBr8-DzppXdgm8",
+    authDomain: "story-telling-app-a1cf4.firebaseapp.com",
+    projectId: "story-telling-app-a1cf4",
+    storageBucket: "story-telling-app-a1cf4.appspot.com",
+    messagingSenderId: "3513387491",
+    appId: "1:3513387491:web:d337eeea6181fc7ad978d6"
+  };
